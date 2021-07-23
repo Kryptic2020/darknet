21cad37620d7145d41ae3620e09b7badd09cf30c
Edson Nascimento <aquenzitech@gmail.com>
2021-07-23 21:23:34 +1000

    added home page with searching and filtering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,35 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [:restricted]
-  
+  before_action :authenticate_user!, only: [:restricted]  
+
   def home
+    @categories = Category.all
+    @product = Product.all
+    if params[:category] != ""
+      category = params[:category]
+    end
+    if params[:search] != ""
+      title = params[:search]
+    end
+
+    if category && title
+      @title = Product.where(Product.arel_table[:title].matches("%#{title}%"))
+      @product = []
+      @title.each do |cat|       
+        if cat.category_id.to_s == category
+        @product << cat
+        end
+      end
+    elsif category &&!title
+      @product = Product.where(category:category)
+    elsif !category && title
+      @product = Product.where(Product.arel_table[:title].matches("%#{title}%"))
+    else
+    @product = Product.all
+    end    
   end
 
   def restricted
-  end
+  end 
+   
+  
 end
