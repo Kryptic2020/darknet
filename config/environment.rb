34cd0d2a2c9d38df48d20f544e9efac900b72b35
Edson Nascimento <aquenzitech@gmail.com>
2021-08-08 09:49:40 +1000

    added sendgrid setup to production

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require_relative "application"
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
+  :user_name => ENV["sendgrid_id_key"]
   :password => ENV["sendgrid_api_key"], # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'darknet2.herokuapp.com',
   :address => 'smtp.sendgrid.net',
