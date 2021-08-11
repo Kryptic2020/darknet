73191a793f1923ab4e1a509956c5db9e5ee6ae2a
Edson Nascimento <aquenzitech@gmail.com>
2021-08-11 18:29:51 +1000

    sendgrid production set up updated

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => ENV["sendgrid_api_key"], # This is the secret sendgrid API key which was issued during API key creation
+  :password => Rails.application.credentials.sendgrid_api_key!, # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'gmail.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
